f12e0bc7b56d2719777f45d09920d0dbc73dc412
LudingtonRobotics <LudingtonRobotics@10.95.0.175>
2018-01-18 16:24:48 -0500

    New code

--- src/org/usfirst/frc/team7160/robot/Robot.java
+++ src/org/usfirst/frc/team7160/robot/Robot.java
@@ -12,10 +12,10 @@ import edu.wpi.first.wpilibj.drive.MecanumDrive;
 
 public class Robot extends IterativeRobot {
 
-	WPI_TalonSRX frontLeft = new WPI_TalonSRX(0);
-	WPI_TalonSRX frontRight = new WPI_TalonSRX(0);
-	WPI_TalonSRX backLeft = new WPI_TalonSRX(0);
-	WPI_TalonSRX backRight = new WPI_TalonSRX(0);
+	WPI_TalonSRX frontLeft = new WPI_TalonSRX(1);
+	WPI_TalonSRX frontRight = new WPI_TalonSRX(4);
+	WPI_TalonSRX backLeft = new WPI_TalonSRX(3);
+	WPI_TalonSRX backRight = new WPI_TalonSRX(2);
 	// For normal tank drive-
 	/*
 	 * SpeedControllerGroup left = new SpeedControllerGroup(frontLeft, backLeft);
@@ -37,10 +37,8 @@ public class Robot extends IterativeRobot {
 
 	@Override
 	public void robotInit() {
-		// Might have to do this
-		backLeft.setInverted(true);
-		frontLeft.setInverted(true);
 		compressor.start();
+		mainDrive.setSafetyEnabled(false);
 
 	}
 
@@ -71,14 +69,9 @@ public class Robot extends IterativeRobot {
 		double rot = joy1.getRawAxis(4);
 		if (Math.abs(x) >= 0.05 || Math.abs(y) >= 0.05 || Math.abs(rot) >= 0.05) {
 			mainDrive.driveCartesian(y, x, rot);
-		} /*
-			 * else {
-			 * 
-			 * if (joy1.getRawButton(1)) { mainDrive.driveCartesian(-0.5, 0, 0); } if
-			 * (joy1.getRawButton(2)) { mainDrive.driveCartesian(0, 0.5, 0); } if
-			 * (joy1.getRawButton(3)) { mainDrive.driveCartesian(0, -0.5, 0); } if
-			 * (joy1.getRawButton(4)) { mainDrive.driveCartesian(0.5, 0, 0); } }
-			 */
+		} else {
+			mainDrive.driveCartesian(0, 0, 0);
+		}
 		if (joy1.getRawButton(5)) {
 			clamp.set(DoubleSolenoid.Value.kForward);
 		} else {
